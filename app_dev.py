cd3077fde657e6960f6c71da60b205656d115b30
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2024-10-11 23:28:29 +0200

    ...

--- app_dev.py
+++ app_dev.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('src', 'index.html')
+    return send_from_directory('templates', 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
